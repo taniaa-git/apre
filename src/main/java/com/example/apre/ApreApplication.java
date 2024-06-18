9f6982492f8929947fcd3f5dd4950e4d77b52cc9
taniaa-git <tania.vega.v1@gmail.com>
2024-06-18 15:06:58 -0700

    Plantilla de back

---
+++ src/main/java/com/example/apre/ApreApplication.java
@@ -0,0 +1,13 @@
+package com.example.apre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apre/ApreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
